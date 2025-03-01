8a6d86b84f1abbc15ab180e745044c98fe83b986
Admin <mrwestcodes@gmail.com>
2025-03-01 15:11:42 -0500

    Updated daily data

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HashRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+
+<HashRouter>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <App />
+    </React.StrictMode>
+</HashRouter>
+
+  
 );
 
 
