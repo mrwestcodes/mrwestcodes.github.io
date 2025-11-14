234925c4ba1e2076b9fdc8df9291c5c0374669da
Admin <mrwestcodes@gmail.com>
2025-11-14 09:06:50 -0500

    update components 11-14-2025

--- src/components/Rebounds.tsx
+++ src/components/Rebounds.tsx
@@ -25,7 +25,7 @@ const Rebounds = () => {
   const [data, setData] = useState<ReboundsData[]>([]);
 
   useEffect(() => {
-    fetch("/data/rebounders.json")
+    fetch("/public/data/rebounders.json")
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error loading rebounds data:", error));
